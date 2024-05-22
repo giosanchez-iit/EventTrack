85a76b6e84cd85d894531584d0d2420de717bda1
giosanchez-iit <161739202+giosanchez-iit@users.noreply.github.com>
2024-05-22 11:27:46 +0800

    Basic Interface for Login Page and Main Page Completed

---
+++ main_page.py
@@ -0,0 +1,64 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QSpacerItem, QSizePolicy, QPushButton
+from PyQt5.uic import loadUi
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.setStyleSheet("""background-color:white;""")
+        layout = QHBoxLayout(central_widget)
+
+        #------------------------------------------------------------------------------------------------------------------
+        #   SIDEBAR
+        #------------------------------------------------------------------------------------------------------------------
+        # Add Side Bar to Main Window
+        self.sidebar = QWidget()
+        loadUi('ui/side_bar.ui', self.sidebar)
+
+        # Add the sidebar to the layout
+        layout.addWidget(self.sidebar)
+        layout.setSpacing(0)
+        # Connect the logout button signal to a slot
+        self.sidebar.findChild(QPushButton, 'logout_button').clicked.connect(self.logout)
+        
+        #------------------------------------------------------------------------------------------------------------------
+        #   VBOX FOR TOP BAR, SEARCH BAR, AND TABLE VIEW
+        #------------------------------------------------------------------------------------------------------------------
+        vbox = QVBoxLayout()
+        vbox.setSpacing(0)
+
+        # Load and add top_bar.ui
+        top_bar = QWidget()
+        loadUi('ui/top_bar.ui', top_bar)
+        vbox.addWidget(top_bar)
+
+        # Load and add search_bar.ui
+        search_bar = QWidget()
+        loadUi('ui/search_bar.ui', search_bar)
+        vbox.addWidget(search_bar)
+
+        # Load and add table_view.ui
+        table_view = QWidget()
+        loadUi('ui/table_view.ui', table_view)
+        vbox.addWidget(table_view)
+
+        table_view = QWidget()
+        loadUi('ui/bottom_bar.ui', table_view)
+        vbox.addWidget(table_view)
+        
+        # Add the vbox to the layout
+        layout.addLayout(vbox)
+
+        self.setWindowTitle('Main Window with Sidebar and Spacer')
+
+    def logout(self):
+        print("Logout button clicked!")
+        # Here you can add the logic for logging out
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
